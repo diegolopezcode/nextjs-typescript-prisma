0cf664dba53a8d45f15d265557cd2fa36387be22
Diego Lopez <diegolopez.coder@gmail.com>
2023-10-01 20:48:15 -0500

    Add[Next-priams]: Add CRUD to notes
    What: Create CRUD to notes in Next.js
    Why: To handle the data in the database
    
    002-Add-CRUD-to-notes

--- src/app/api/notes/[id]/route.ts
+++ src/app/api/notes/[id]/route.ts
@@ -1,22 +1,74 @@
 import { NextResponse } from "next/server";
+import { prisma } from '../../../../libs/prisma';
+import { notFound } from "next/navigation";
+import { Prisma } from "@prisma/client";
 
-export const GET = (
-    req: { params: { id: string } },
-) => {
 
-    return NextResponse.json({ message: "Desde Server", id: req.params?.id });
+interface Params {
+ params: { id: string }
+}
+export const GET = async (req:Request,{params}: Params) => {
+  try {
+      const resp= await prisma.note.findUnique({
+          where: {
+              id: Number(params?.id)
+          }
+      })
+
+      if(!resp) {
+          notFound()
+      }
+  
+      return NextResponse.json({resp });
+  } catch (error) {
+    if (error instanceof Error) {
+        return NextResponse.json({ message: error.message, statusCode: 500 });
+        }
+  }
 }
 
-export const DELETE = (
-    req: { params: { id: string } },
-) => {
+export const DELETE = async (req :Request, {params}: Params) => {
+    try {
+      const resp = await prisma.note.delete({
+        where: {
+          id: Number(params.id)
+        }
+      })
+  
+    
+      return NextResponse.json({ resp });
+      
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if(error.code === 'P2025') {
+            notFound()
+            }
 
-    return NextResponse.json({ message: "Deleting things ...", id: req.params?.id });
-}
+        return NextResponse.json({ message: error.message, statusCode: 500 });
+      }
+    }
+  }
 
-export const PUT = (
-    req: { params: { id: string } },
-) => {
-
-    return NextResponse.json({ message: "Updating things ...", id: req.params?.id });
+export const PUT = async (req: Request, { params }: Params) => {
+    try {
+        const { title, content } = await req.json();
+        const resp = await prisma.note.update({
+            where: {
+                id: Number(params.id)
+            },
+            data: {
+                title,
+                content
+            }
+        })
+       
+        return NextResponse.json({ resp });
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if(error.code === 'P2025') {
+                notFound()
+                }
+            return NextResponse.json({ message: error.message, statusCode: 500 });
+        }
+    }
 }
--- src/app/api/notes/route.ts
+++ src/app/api/notes/route.ts
@@ -1,9 +1,36 @@
 import { NextResponse } from "next/server";
+import { prisma } from "../../../libs/prisma";
+import { notFound } from "next/navigation";
 
-export const GET = () => {
-  return NextResponse.json({ message: "Desde Server" });
+interface Params {
+  params: {id: string}  
+}
+
+export const GET = async () => {
+  try {
+    const listNotes = await prisma.note.findMany();
+    return NextResponse.json(listNotes);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message, statusCode: 500 });
+    }
+  }
 };
 
-export const POST = () => {
-  return NextResponse.json({ message: "Creating things ..." });
+export const POST = async (req: Request) => {
+  try {
+    const { title, content } = await req.json();
+    const resp = await prisma.note.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    return NextResponse.json({ resp });
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message, statusCode: 500 });
+    }
+  }
+
 };
